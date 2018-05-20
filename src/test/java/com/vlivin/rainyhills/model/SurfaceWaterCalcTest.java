7ba356e25f6a8a89a6f5feda3a602654b93c2a34
Vladimir Livin <vladimir@livesystems.ru>
2018-05-21 01:06:05 +0300

    SurfaceWaterCalcTest.java total water remained tests converted to parametrized test.
    Previously we had a couple of test methods looking similarly having input as profile and checking if it has correct water remained after rain. Now these methods replaced with just one @ParameterizedTest calculateWaterVolume with profile and expectedVolume as input - it uses surfaceProfileAndCalculatedWaterVolumeFixtures as a source input method. THis way we share same test method behavior but with different input and expected output.

--- src/test/java/com/vlivin/rainyhills/model/SurfaceWaterCalcTest.java
+++ src/test/java/com/vlivin/rainyhills/model/SurfaceWaterCalcTest.java
@@ -2,6 +2,11 @@ package com.vlivin.rainyhills.model;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,6 +26,21 @@ class SurfaceWaterCalcTest {
         calc = new SurfaceWaterCalc(false);
     }
 
+    @ParameterizedTest
+    @MethodSource("surfaceProfileAndCalculatedWaterVolumeFixtures")
+    void calculateWaterVolume(int[] profile, int expectedVolume) {
+        assertEquals(expectedVolume, calc.calculateWaterVolume(new Surface(profile)).getTotal());
+    }
+
+    private static Stream<Arguments> surfaceProfileAndCalculatedWaterVolumeFixtures() {
+        return Stream.of(
+                Arguments.of(new int[]{3, 2, 4, 1, 2}, 2),
+                Arguments.of(new int[]{4, 1, 1, 0, 2, 3}, 8),
+                Arguments.of(new int[]{5, 3, 7, 2, 6, 4, 5, 9, 1, 2}, 14),
+                Arguments.of(new int[]{1, 2, 1, 1, 4, 1, 1, 2, 5, 3, 4}, 11)
+        );
+    }
+
     @Test
     void calculateWaterVolume_whenEmpty_shouldBeZero() {
         assertEquals(0, calc.calculateWaterVolume(new Surface(new int[] {})).getTotal());
@@ -32,26 +52,11 @@ class SurfaceWaterCalcTest {
     }
 
     @Test
-    void calculateWaterVolume_when32412_shouldBe2() {
-        assertEquals(2, calc.calculateWaterVolume(new Surface(new int[] {3, 2, 4, 1, 2})).getTotal());
-    }
-
-    @Test
     void calculateWaterVolume_when411023_shouldBeTotal8_withWaterRemained022310() {
         calc = new SurfaceWaterCalc(true);
         WaterVolumeResult waterVolumeResult = calc.calculateWaterVolume(new Surface(new int[]{4, 1, 1, 0, 2, 3}));
         assertEquals(8, waterVolumeResult.getTotal());
         assertArrayEquals(new int[] {0, 2, 2, 3, 1, 0}, waterVolumeResult.getRemainedAt());
-    }
-
-    @Test
-    void calculateWaterVolume_when5372645912_shouldBe14() {
-        assertEquals(14, calc.calculateWaterVolume(new Surface(new int[] {5,3,7,2,6,4,5,9,1,2})).getTotal());
-    }
-
-    @Test
-    void calculateWaterVolume_when12114112534_shouldBe11() {
-        assertEquals(11, calc.calculateWaterVolume(new Surface(new int[] {1, 2, 1, 1, 4, 1, 1, 2, 5, 3, 4})).getTotal());
     }
 
     @Test
